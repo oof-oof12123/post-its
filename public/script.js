e1e774213d722d4d3873b3a12caedda06f3ca524
oof-oof12123 <kevin.alistairang@gmail.com>
2026-06-07 11:34:37 +0700

    Implement loadMessages and submitMessage functions
    Added functions to load and submit messages to the guestbook.

--- public/script.js
+++ public/script.js
@@ -1,1 +1,49 @@
+async function loadMessages() {
+    const response = await fetch("/messages");
+    const messages = await response.json();
 
+    const guestbook =
+        document.getElementById("guestbook");
+
+    guestbook.innerHTML = "";
+
+    for (const msg of messages) {
+        guestbook.innerHTML += `
+            <div class="message">
+                <div class="name">${msg.name}</div>
+                <div>${msg.message}</div>
+                <div class="date">${msg.date}</div>
+            </div>
+        `;
+    }
+}
+
+async function submitMessage() {
+    const name =
+        document.getElementById("name").value.trim();
+
+    const message =
+        document.getElementById("message").value.trim();
+
+    if (!name || !message) {
+        alert("yo either your name isnt down there or you didnt write anything. write it!!! (just write anonymous if you want)");
+        return;
+    }
+
+    await fetch("/messages", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            name,
+            message
+        })
+    });
+
+    document.getElementById("message").value = "";
+
+    loadMessages();
+}
+
+loadMessages();
